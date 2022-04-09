5e159828e0c974fde4be188c3ec9604195615d44
dilapitan <domlapitan@gmail.com>
2022-04-09 14:23:35 +0800

    Update layout in Home page

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,41 @@
         <h3>Get ready for AWS!</h3>
       </v-toolbar-title>
 
+      <v-btn @click="toggleTheme()" icon color="accented">
+        <v-icon v-if="!$vuetify.theme.dark">mdi-weather-night</v-icon>
+        <v-icon v-else>mdi-white-balance-sunny</v-icon>
+      </v-btn>
+
       <v-spacer></v-spacer>
 
-      <v-btn icon large>
-        <v-icon>mdi-information-outline</v-icon>
-      </v-btn>
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="showAbout()" icon large v-bind="attrs" v-on="on">
+            <v-icon>mdi-information-outline</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title> About this app </v-card-title>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="dialog = false"> Close </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-app-bar>
 
     <v-card color="background" flat height="100%">
@@ -35,7 +65,23 @@ export default {
   },
 
   data: () => ({
-    //
+    dialog: false,
   }),
+
+  created() {
+    const theme = localStorage.getItem('dark_theme')
+    this.$vuetify.theme.dark = theme === 'true' ? true : false
+  },
+
+  methods: {
+    showAbout() {
+      this.drawer = true
+    },
+
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.setItem('dark_theme', this.$vuetify.theme.dark.toString())
+    },
+  },
 }
 </script>
