319ddeb9e411621cd9684e42fa3bdab2ccce5446
dilapitan <domlapitan@gmail.com>
2022-04-09 22:01:57 +0800

    Add content to About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,109 @@
 <template>
   <v-container>
-    <v-card-text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </v-card-text>
+    <p class="font-weight-bold font-italic">Q: Why did I build this app?</p>
+    <p class="text-body-2 ml-4">
+      : Because I'm studying AWS SAA and wanted to be certified on it. And using
+      Active Recall and Space Repetition are the best ways to study on this type
+      of exam. 🙂
+    </p>
+
+    <br />
+
+    <p class="font-weight-bold font-italic">
+      Q: Aren't there other websites for online flashcards already?
+    </p>
+    <p class="text-body-2 ml-4">
+      : Yes, indeed! But my notes are in Notion and wanted to stay that way. And
+      also I have the chance to finally study and use Notion API. 😌
+    </p>
+
+    <br />
+
+    <p class="font-weight-bold font-italic">
+      Q: Where did your notes come from?
+    </p>
+    <p class="text-body-2 ml-4">
+      : From a Udemy course. Here's the
+      <span>
+        <a
+          class="accented--text"
+          href="https://www.udemy.com/share/102CPB3@USDi0atYr3hR0c497q3Wim6u1V92dr8XXv2nNwj5rn2OgC0GkFsAY4ywUKPiuYNM/"
+          target="_blank"
+        >
+          link</a
+        >.
+      </span>
+      <span
+        >I also plan to expand my notes and knowledge by enrolling to
+        <a
+          class="accented--text"
+          href="https://twitter.com/acloudguru"
+          target="_blank"
+          >ACloud Guru</a
+        >! 🤓
+      </span>
+    </p>
+
+    <br />
+
+    <p class="font-weight-bold font-italic">
+      Q: Can we see the database of your notes for this app?
+    </p>
+    <p class="text-body-2 ml-4">
+      : Yes, of course! Here's the Notion
+      <span>
+        <a
+          class="accented--text"
+          href="https://dilapitan.notion.site/8c5e21ae071d40f4be311c6529546ed8?v=0a54d19acea74f89b18bebc763abdce6"
+          target="_blank"
+        >
+          link</a
+        >. 📝
+      </span>
+    </p>
+
+    <br />
+
+    <p>
+      <span>Built using </span>
+      <span
+        ><a class="accented--text" href="https://vuejs.org/" target="_blank"
+          >Vue</a
+        >
+        with
+        <a
+          class="accented--text"
+          href="https://vuetifyjs.com/en/"
+          target="_blank"
+          >Vuetify</a
+        >,
+      </span>
+      <span
+        ><a class="accented--text" href="https://nodejs.org/en/" target="_blank"
+          >Node</a
+        ></span
+      >
+      <span> and </span>
+      <span
+        ><a class="accented--text" href="https://expressjs.com/" target="_blank"
+          >Express</a
+        >, and of course
+      </span>
+      <span
+        ><a
+          class="accented--text"
+          href="https://developers.notion.com/"
+          target="_blank"
+          >Notion API</a
+        >!!!
+      </span>
+      <span>😀😀😀</span>
+    </p>
+
+    <br />
+    <p>
+      📬 Contact me at:
+      <span class="accented--text">dilapitan.dev@gmail.com</span>
+    </p>
   </v-container>
 </template>
