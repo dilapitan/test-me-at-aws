3bd02b410a6ec341db5805c5617a4b2dd2a3e863
dilapitan <domlapitan@gmail.com>
2022-04-09 14:46:08 +0800

    Add Flashcard

---
+++ src/components/Flashcard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>
+    <v-card-title>{{ details.question }}</v-card-title>
+    <v-card-text>
+      {{ details.answers }}
+    </v-card-text>
+
+    <p>{{ details.category.name }}</p>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Flashcard',
+  props: {
+    details: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,44 @@
 <template>
   <div>
-    Hello
+    <br /><br />
+    <v-row>
+      <v-col cols="1" sm="3"></v-col>
+      <v-col cols="10" sm="6">
+        <Flashcard :details="details" />
+
+        <br />
+        <v-row class="pr-3">
+          <v-spacer></v-spacer>
+          <v-btn class="white--text" color="accented">NEXT</v-btn>
+        </v-row>
+      </v-col>
+      <v-col cols="1" sm="3"></v-col>
+    </v-row>
   </div>
 </template>
+
+<script>
+import Flashcard from '@/components/Flashcard'
+
+export default {
+  name: 'Home',
+
+  components: {
+    Flashcard,
+  },
+
+  data: () => ({
+    flashcards: null,
+    details: {
+      question: 'What are availability zones (AZ)?',
+      answers:
+        '- the ones that are going into the region\n- one or more discrete data centers with redundant power, networking, and connectivity\n- separate from each other; isolated from disaster\n- connected to each other with super awesome networking',
+      category: {
+        id: '8a9b319d-1ea1-4460-aaa5-d5db8618e247',
+        name: 'AWS Cloud Overview',
+        color: 'brown',
+      },
+    },
+  }),
+}
+</script>
