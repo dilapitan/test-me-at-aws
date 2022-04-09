bccea41b1d71b3555b9989f319018cd4fe70565e
dilapitan <domlapitan@gmail.com>
2022-04-09 14:24:40 +0800

    Add About component

--- src/App.vue
+++ src/App.vue
@@ -26,15 +26,7 @@
         <v-card>
           <v-card-title> About this app </v-card-title>
 
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </v-card-text>
+          <About />
 
           <v-divider></v-divider>
 
@@ -55,12 +47,14 @@
 </template>
 
 <script>
+import About from '@/components/About'
 import Home from '@/components/Home'
 
 export default {
   name: 'App',
 
   components: {
+    About,
     Home,
   },
 
