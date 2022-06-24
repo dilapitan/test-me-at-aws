4a758856ee395126020bacf5c67a33557356edc4
dilapitan <domlapitan@gmail.com>
2022-06-24 14:19:47 +0800

    Deploy

---
+++ precache-manifest.ba693aba399b18b724bfc003afee485b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42450d4554f15f3a501",
+    "url": "/test-me-at-aws/css/chunk-vendors.d58f19cc.css"
+  },
+  {
+    "revision": "4257fa0e5f92317e0fef3a3bf0ae010e",
+    "url": "/test-me-at-aws/index.html"
+  },
+  {
+    "revision": "35515b5b6264844fbbe3",
+    "url": "/test-me-at-aws/js/app.78b768ef.js"
+  },
+  {
+    "revision": "e42450d4554f15f3a501",
+    "url": "/test-me-at-aws/js/chunk-vendors.4238a98a.js"
+  },
+  {
+    "revision": "35c65c7e7b797246c4e77ea292328bc3",
+    "url": "/test-me-at-aws/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-me-at-aws/robots.txt"
+  }
+]);
